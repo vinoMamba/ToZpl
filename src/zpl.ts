50f25c705366ed07ec1f7ea3691e8f6449cc54de
vino <vino0908@outlook.com>
2023-08-28 21:36:30 +0800

    refactor: create zpl.ts

---
+++ src/zpl.ts
@@ -0,0 +1,30 @@
+import { ZplEnum } from './Command'
+import type { GbOptions } from './type'
+import { formatZpl } from './utils'
+
+export class Zpl {
+  private value: string
+  private zplList: Array<string>
+  constructor() {
+    this.value = ''
+    this.zplList = []
+  }
+
+  gb(top: number, left: number, opt: GbOptions) {
+    const { width, height, borderWidth = 1 } = opt
+    const gbItem = `
+      ${ZplEnum.FIELD_ORIGIN}${top},${left}
+      ${ZplEnum.GRAPHIC_BOX}${width},${height},${borderWidth},B,0${ZplEnum.SEPARATOR}`
+    this.zplList.push(gbItem)
+  }
+
+  getZpl() {
+    const resultList = [
+      ZplEnum.START,
+      ...this.zplList,
+      ZplEnum.END,
+    ]
+    const zplStr = resultList.join('')
+    return formatZpl(zplStr)
+  }
+}
